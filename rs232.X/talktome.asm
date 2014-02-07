96797a94e7fb6099b40fb798c090104cef40e717
d-sci <sharesindavid@gmail.com>
2014-02-07 01:39:39 -0500

    working on RS232 sample code

--- rs232.X/talktome.asm
+++ rs232.X/talktome.asm
@@ -5,6 +5,16 @@
         include     <p16f877.inc>
         __CONFIG _CP_OFF & _WDT_OFF & _BODEN_ON & _PWRTE_ON & _HS_OSC & _WRT_ENABLE_ON & _CPD_OFF & _LVP_OFF
 
+writetoPC   macro      lit
+        bcf       STATUS,RP0     ; Go back to bank 0
+        movlw       lit
+        movwf     TXREG          ; Send contents of W to RS232
+        bsf       STATUS,RP0     ; Go to bank with TXSTA
+        btfss     TXSTA,1        ; check TRMT bit in TXSTA (FSR) until TRMT=1
+        goto      $-1
+        endm
+
+
 offset  EQU       0x20
 temp    EQU       0x21
 
@@ -26,38 +36,38 @@ init    bsf       STATUS,RP0     ; select bank 1
         bsf       TXSTA,TXEN     ; Transmit enable
 
 ;********** Send welcome message **********************
-        bcf       STATUS,RP0     ; select bank 0
-        clrf      offset         ; Reset offset to 0
 
-start   movf      offset,w       ; Load TAB[offset] into W
-        call      TAB
-        addlw     0              ; Character = 00 ?
-        btfsc     STATUS,Z       ; (Adding 0 causes Z bit to be set if W contains 0)
-        goto      run            ; Yes , Z = 1 => We're done sending welcome message
-                                 ; No  , Z = 0 => Keep sending welcome message
 
-        movwf     TXREG          ; Send contents of W to RS232
+    writetoPC   "O"
+    writetoPC   "p"
+    writetoPC   "e"
+    writetoPC   "r"
+    writetoPC   "a"
+    writetoPC   "t"
+    writetoPC   "i"
+    writetoPC   "o"
+    writetoPC   "n"
+    writetoPC   " "
+    writetoPC   "a"
+    writetoPC   "t"
+    writetoPC   ":"
+    writetoPC   D'1'
 
-        bsf       STATUS,RP0     ; Go to bank with TXSTA
-        btfss     TXSTA,1        ; check TRMT bit in TXSTA (FSR) until TRMT=1
-        goto      $-1
-        bcf       STATUS,RP0     ; Go back to bank 0
-
-        incf      offset,f       ; Move to next character in welcome message and repeat
-        goto      start
+      goto  $
+        
 
 ;********** Start echoing characters back on RS232 *******************
-run     btfss     PIR1,RCIF      ; Check RCIF  bit in PIR1 register until RCIF=1
-        goto      $-1
-
-        movf      RCREG,w        ; Read the character received from RS232
-        movwf     TXREG          ; Write it right back to RS232
-
-        goto      run
+;run     btfss     PIR1,RCIF      ; Check RCIF  bit in PIR1 register until RCIF=1
+;        goto      $-1
+;
+;        movf      RCREG,w        ; Read the character received from RS232
+;        movwf     TXREG          ; Write it right back to RS232
+;
+;        goto      run
 
 ;********* Table containing welcome message ********************
-TAB     addwf     PCL,f            ; Move offset to PC lower
-        dt  "Hello David, This is PICtricia talking.",0
+;TAB     addwf     PCL,f            ; Move offset to PC lower
+;        dt  "Hello David, This is PICtricia talking.",0
 
         end
 
